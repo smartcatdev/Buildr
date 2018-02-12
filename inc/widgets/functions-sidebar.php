66ccf50f116fe088fb594bc2326c1ccd26fb6b21
smartcatdev <bilal@smartcat.ca>
2018-02-12 09:33:27 -0500

    updated sidebar

--- inc/widgets/functions-sidebar.php
+++ inc/widgets/functions-sidebar.php
@@ -13,7 +13,7 @@ function designr_widgets_init() {
     
     register_sidebar( array (
         'name' => esc_html__( 'Sidebar', 'designr' ),
-        'id' => 'sidebar-1',
+        'id' => 'sidebar-right',
         'description' => esc_html__( 'Add widgets here.', 'designr' ),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget' => '</section>',
