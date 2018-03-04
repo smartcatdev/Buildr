7903d6c2b37cd551600b95c31c3f04941834c5f8
Zack Mitchell <zackcmitchell@gmail.com>
2018-03-04 17:05:01 -0500

    Quick Fix removing forced Custom Header W&H

--- inc/functions-custom-header.php
+++ inc/functions-custom-header.php
@@ -20,8 +20,6 @@ function designr_custom_header_setup() {
     add_theme_support( 'custom-header', apply_filters( 'designr_custom_header_args', array (
         'default-image' => '',
         'default-text-color' => '000000',
-        'width' => 1000,
-        'height' => 250,
         'flex-height' => true,
         'wp-head-callback' => 'designr_header_style',
     ) ) );
