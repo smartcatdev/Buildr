d043a4d0ebedbebf74ab27e5e800b7e7fddb02fe
Zack Mitchell <zackcmitchell@gmail.com>
2018-03-29 11:52:31 -0400

    Commented out default sidebar declaration

--- inc/functions-widgets.php
+++ inc/functions-widgets.php
@@ -10,15 +10,15 @@ add_action( 'widgets_init', 'designr_widgets_init' );
  */
 function designr_widgets_init() {
     
-    register_sidebar( array (
-        'name'              => esc_html__( 'Sidebar A', 'designr' ),
-        'id'                => 'sidebar-a',
-        'description'       => esc_html__( 'Add widgets here.', 'designr' ),
-        'before_widget'     => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'      => '</section>',
-        'before_title'      => '<h2 class="widget-title">',
-        'after_title'       => '</h2>',
-    ) );
+//    register_sidebar( array (
+//        'name'              => esc_html__( 'Sidebar A', 'designr' ),
+//        'id'                => 'sidebar-a',
+//        'description'       => esc_html__( 'Add widgets here.', 'designr' ),
+//        'before_widget'     => '<section id="%1$s" class="widget %2$s">',
+//        'after_widget'      => '</section>',
+//        'before_title'      => '<h2 class="widget-title">',
+//        'after_title'       => '</h2>',
+//    ) );
 
     register_sidebar( array (
         'name'              => esc_html__( 'Post - Above Content', 'designr' ),
