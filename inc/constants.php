a144b7f67b0f7aee1a839a871329bc37e81d3a21
smartcatdev <bilal@smartcat.ca>
2018-03-29 11:50:05 -0400

    added constants

---
+++ inc/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+interface DESIGNR_OPTIONS {
+    
+    const CUSTOM_HEADER_STYLE_TOGGLE = 'custom_header_style_toggle';
+    
+    
+    
+}
+
+interface DESIGNR_DEFAULTS {
+    
+    const CUSTOM_HEADER_STYLE_TOGGLE = 'parallax_vertical';
+    
+}
