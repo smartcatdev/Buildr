5dd2f4515a1ed3f5e2342ff74aba96933f2bba27
Zack Mitchell <zackcmitchell@gmail.com>
2018-04-16 12:47:17 -0400

    Before Buildr name change

---
+++ admin/doc-partials/page_templates.php
@@ -0,0 +1,16 @@
+<section class="sub-section">
+
+    <h3 id="page-templates" class="sub-heading">
+        <?php _e( 'Page Templates', 'designr' ); ?>
+    </h3>
+    
+    <p>
+        <?php _e( 'Designr includes 6 Page Templates that can be used to create unique arrangements of widgets that only display on their respective Pages. Of those six templates, three are Landing Pages (these have a single widget area, and do not output page content). The other three are standard Page Templates that output the content between two widget areas (above & below the content).', 'designr' ); ?>
+    </p>
+    <p>
+        <?php _e( 'As an example, if you create a new page, you could set its template to "Page Template A", then add widgets to its two widget areas, "Page Template A - Above Content" and "Page Template A - Below Content". The widgets added within those areas will only output on that page, set as "Page Template A".', 'designr' ); ?>
+    </p>
+    
+    <img alt="<?php _e( 'Page Templates', 'designr' ); ?>" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/docs/page_templates.jpg' ); ?>">
+    
+</section>
