51a3bae805c24702ba54144c3444c12dba01a1a9
Zack Mitchell <zackcmitchell@gmail.com>
2018-04-18 11:31:17 -0400

    QA CHECKPOINT

--- inc/functions-css.php
+++ inc/functions-css.php
@@ -19,7 +19,8 @@ function buildr_wp_head_styles() { ?>
         h2.widget-title,
         ul.product_list_widget .product-wrap .product-title .price-wrap,
         #buildr-featured-woocommerce h4.product_category_title,
-        div#buildr-woocommerce-wrap ul.products li.product .product_category_title {
+        div#buildr-woocommerce-wrap ul.products li.product .product_category_title,
+        div#buildr-woocommerce-wrap ul.products li.product .woocommerce-loop-category__title mark {
             font-family: <?php echo esc_attr( get_theme_mod( BUILDR_OPTIONS::FONT_SECONDARY, BUILDR_DEFAULTS::FONT_SECONDARY ) ); ?>;
         }
 
