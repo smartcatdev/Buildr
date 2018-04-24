f58d335b8b3a5470386a57d4b9e90d23920068ea
Zack Mitchell <zackcmitchell@gmail.com>
2018-04-24 13:49:23 -0400

    Updated and designed search.php template

--- search.php
+++ search.php
@@ -7,45 +7,105 @@
  * @package Buildr
  */
 
-get_header(); ?>
+get_header();
+?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+    
+    <main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) : ?>
+        <div class="padded-content-wrap">
+        
+            <?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'buildr' ), '<span>' . get_search_query() . '</span>' );
-				?></h1>
-			</header><!-- .page-header -->
+                <div class="container">
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+                    <div class="row">
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+                        <div class="col-sm-12">
 
-			endwhile;
+                            <header class="page-header">
+                                <h1 class="page-title">
+                                    <?php printf( esc_html__( 'Search Results for: %s', 'buildr' ), '<span>' . get_search_query() . '</span>' ); ?>
+                                </h1>
+                            </header><!-- .page-header -->
 
-			the_posts_navigation();
+                        </div>
 
-		else :
+                    </div>
 
-			get_template_part( 'template-parts/content', 'none' );
+                </div>
 
-		endif; ?>
+                <?php 
+                switch ( get_theme_mod( BUILDR_OPTIONS::BLOG_LAYOUT_STYLE, BUILDR_DEFAULTS::BLOG_LAYOUT_STYLE ) ) :
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+                    case 'blog_masonry' :
+                        do_action( 'buildr_blog_masonry_wrap_open');
+                        break;
 
-<?php
-get_sidebar();
-get_footer();
+                    case 'blog_mosaic' :
+                        do_action( 'buildr_blog_mosaic_wrap_open');
+                        break;
+
+                    default :
+                        do_action( 'buildr_blog_standard_wrap_open');
+
+                endswitch; 
+                ?>
+
+                    <?php
+                    /* Start the Loop */
+                    while ( have_posts() ) : the_post();
+
+                        /*
+                         * Include the Post-Format-specific template for the content.
+                         * If you want to override this in a child theme, then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                        get_template_part( 'template-parts/content', get_theme_mod( BUILDR_OPTIONS::BLOG_LAYOUT_STYLE, BUILDR_DEFAULTS::BLOG_LAYOUT_STYLE ) );
+
+                    endwhile;
+                    ?>
+
+                <?php 
+                switch ( get_theme_mod( BUILDR_OPTIONS::BLOG_LAYOUT_STYLE, BUILDR_DEFAULTS::BLOG_LAYOUT_STYLE ) ) :
+
+                    case 'blog_masonry' :
+                        do_action( 'buildr_blog_masonry_wrap_close');
+                        break;
+
+                    case 'blog_mosaic' :
+                        do_action( 'buildr_blog_mosaic_wrap_close');
+                        break;
+
+                    default :
+                        do_action( 'buildr_blog_standard_wrap_close');
+
+                endswitch; 
+                ?>
+
+            <?php else : ?>
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-sm-12">
+
+                            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+            <?php endif; ?>
+            
+        </div>
+
+    </main><!-- #main -->
+    
+</div><!-- #primary -->
+
+<?php get_footer();
