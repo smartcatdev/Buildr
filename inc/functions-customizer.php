08ccb70a2cb16706c74b8383c32f568b2811f5a4
Zack Mitchell <zackcmitchell@gmail.com>
2018-04-27 11:41:49 -0400

    Added two esc_url() wrappers

--- inc/functions-customizer.php
+++ inc/functions-customizer.php
@@ -3,7 +3,7 @@
 function buildr_theme_customize_controls_js() {
     
     wp_localize_script( 'buildr-theme-customizer-control', 'buildr_customize', array(
-        'ajax_url'                  => admin_url( 'admin-ajax.php' ),
+        'ajax_url'                  => esc_url( admin_url( 'admin-ajax.php' ) ),
         'buildr_dismiss_nonce'      => wp_create_nonce( 'buildr_dismiss_nonce' )
     ) );
     
