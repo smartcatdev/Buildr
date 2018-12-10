d22c895bb3cb72c6acc6b01ca818267ef2974f88
smartcatdev <bilal@smartcat.ca>
2018-12-10 10:18:55 -0500

    updates to buildr for Woo and EDD

--- single-download.php
+++ single-download.php
@@ -19,9 +19,57 @@ get_header(); ?>
 
             endwhile; // End of the loop.
             ?>
-            
+
         </main><!-- #main -->
-        
+
+        <div class="sidebar-wrap post below">
+
+            <div id="edd-header-wrap-wrapper">
+
+            <div id="edd-header-wrap-blurb">
+
+                <?php if ( get_post_meta( get_the_ID(), BUILDR_META::EDD_CATEGORY, true ) ) : ?>
+
+                    <span class="product-category">
+                        <?php echo esc_html( get_post_meta( get_the_ID(), BUILDR_META::EDD_CATEGORY, true ) ); ?>
+                    </span>
+
+                <?php endif; ?>
+
+                <?php the_title( '<h3 class="product_title entry-title">Buy ', ' today!</h3>' ); ?>
+
+                <span class="small-divider dark"></span>
+
+                <div class="edd-buttons">
+
+                    <div class="product-buttons">
+
+                        <?php echo edd_get_purchase_link( array(
+                            'download_id'       => get_the_ID(),
+                            'price'             => false,
+                            'direct'            => false,
+                            'class'             => 'primary',
+                        ) ); ?>
+
+                        <?php if ( get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_LABEL, true ) && get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_URL, true ) ) : ?>
+
+                            <a class="button secondary" href="<?php echo esc_url( get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_URL, true ) ); ?>" <?php echo get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_TARGET, true ) != 'same' ? ' target="_BLANK" ' : ''; ?>>
+                                <?php echo esc_html( get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_LABEL, true ) ); ?>
+                            </a>
+
+                        <?php endif; ?>
+
+                    </div>
+
+                    <div class="clear"></div>
+
+                </div>
+
+        </div>
+            </div>
+            </div>
+
+
     </div><!-- #primary -->
 
 <?php
--- template-parts/content-download.php
+++ template-parts/content-download.php
@@ -22,25 +22,25 @@ $edd_gallery = get_post_meta( get_the_ID(), BUILDR_META::EDD_GALLERY, true );
                 <div class="col-sm-12">
 
                     <div id="edd-header-wrap-wrapper">
-                        
+
                         <div id="edd-header-wrap-blurb">
 
                             <?php if ( get_post_meta( get_the_ID(), BUILDR_META::EDD_CATEGORY, true ) ) : ?>
-                                
+
                                 <span class="product-category">
                                     <?php echo esc_html( get_post_meta( get_the_ID(), BUILDR_META::EDD_CATEGORY, true ) ); ?>
                                 </span>
-                            
+
                             <?php endif; ?>
-                            
+
                             <?php the_title( '<h1 class="product_title entry-title">', '</h1>' ); ?>
 
                             <span class="small-divider dark"></span>
-                            
+
                             <?php if ( function_exists( 'edd_price' ) ) : ?>
-                            
+
                                 <?php $sale_price = get_post_meta( get_the_ID(), 'edd_sale_price', true ); ?>
-                                
+
                                 <div class="price <?php echo isset( $sale_price ) && !empty( $sale_price ) && ! edd_has_variable_prices( get_the_ID() ) ? 'on-sale' : ''; ?> ">
                                     <?php if ( edd_has_variable_prices( get_the_ID() ) ) :
                                         echo edd_price_range( get_the_ID() );
@@ -50,48 +50,48 @@ $edd_gallery = get_post_meta( get_the_ID(), BUILDR_META::EDD_GALLERY, true );
                                 </div>
 
                             <?php endif; ?>
-                            
+
                             <?php the_excerpt(); ?>
-                            
+
                             <div class="edd-buttons">
-                            
+
                                 <div class="product-buttons">
-                                    
+
                                     <?php echo edd_get_purchase_link( array(
                                         'download_id'       => get_the_ID(),
                                         'price'             => false,
                                         'direct'            => false,
                                         'class'             => 'primary',
                                     ) ); ?>
-                                    
+
                                     <?php if ( get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_LABEL, true ) && get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_URL, true ) ) : ?>
-                                    
+
                                         <a class="button secondary" href="<?php echo esc_url( get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_URL, true ) ); ?>" <?php echo get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_TARGET, true ) != 'same' ? ' target="_BLANK" ' : ''; ?>>
                                             <?php echo esc_html( get_post_meta( get_the_ID(), BUILDR_META::EDD_SECOND_BUTTON_LABEL, true ) ); ?>
                                         </a>
-                                    
+
                                     <?php endif; ?>
-                                    
+
                                 </div>
-                                
+
                                 <div class="clear"></div>
 
                             </div>
-                            
+
                         </div>
-                        
+
                         <?php if ( get_post_meta( get_the_ID(), BUILDR_META::EDD_VIDEO_ID, true ) ) : ?>
-                        
+
                             <iframe class="edd-product-video" id="ytplayer" type="text/html" width="640" height="360"
                             src="https://www.youtube.com/embed/<?php echo esc_attr( get_post_meta( get_the_ID(), BUILDR_META::EDD_VIDEO_ID, true ) ); ?>?autoplay=<?php echo get_post_meta( get_the_ID(), BUILDR_META::EDD_VIDEO_AUTOPLAY, true ) == 'autoplay' ? 1 : 0; ?>"
                             frameborder="0"></iframe>
-                        
+
                         <?php else : ?>
 
                             <?php buildr_output_edd_product_gallery( $edd_gallery ); ?>
-                        
+
                         <?php endif; ?>
-                        
+
                     </div>
 
                 </div>
